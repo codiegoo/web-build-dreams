0ae74b1c2f66b5760fc724336b3df44a1a7a8481
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2025-01-26 16:20:07 -0600

    add navBurger

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,33 +1,87 @@
-'use client'
-import Image from "next/image";
-import Link from "next/link";
-import './nav.sass'
-import { useState, useEffect } from "react"
+'use client';
 
+import Image from "next/image";
+import './nav.sass';
+import { useState, useEffect } from "react";
+import NavBurger from "../NavBurger/NavBurger";
 
 export default function Nav() {
+  const [activeLink, setActiveLink] = useState("#InicioContain");
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 990);
+    };
 
-  const [ activeLink, setActiveLink ] = useState("#InicioContain")
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleLinkClick = (href) => {
-    setActiveLink(href)
-  }
-  
+    setActiveLink(href);
+    setIsBurgerOpen(false);
+  };
 
-  return(
-    <nav>
-      <Image width={150} height={40} src="/images/logo.jpeg"/>
-      <ul>
-        <li><Link href="#InicioContain" className={activeLink === "#InicioContain" ? "button" : ""}
-            onClick={() => handleLinkClick("#InicioContain")}>Inicio</Link></li>
-        <li><Link href="#ServiciosContain" className={activeLink === "#ServiciosContain" ? "button" : ""}
-            onClick={() => handleLinkClick("#ServiciosContain")}>Servicios</Link></li>
-        <li><Link href="#NosotrosContain" className={activeLink === "#NosotrosContain" ? "button" : ""}
-            onClick={() => handleLinkClick("#NosotrosContain")}>Nosotros</Link></li>
-        <li><Link href="#ContactoContain" className={activeLink === "#ContactoContain" ? "button" : ""}
-            onClick={() => handleLinkClick("#ContactoContain")}>Contacto</Link></li>
-      </ul>
+  const toggleBurgerMenu = () => {
+    setIsBurgerOpen(!isBurgerOpen);
+  };
+
+  return (
+    <nav className="nav">
+      <Image width={150} height={40} src="/images/logo.jpeg" alt="Logo" />
+      {isSmallScreen && (
+        <NavBurger
+          activeLink={activeLink}
+          handleLinkClick={handleLinkClick}
+          isBurgerOpen={isBurgerOpen}
+          toggleBurgerMenu={toggleBurgerMenu}
+        />
+      )}
+
+      {!isSmallScreen && (
+        <ul className="nav-list">
+          <li>
+            <a
+              href="#InicioContain"
+              className={activeLink === "#InicioContain" ? "button" : ""}
+              onClick={() => handleLinkClick("#InicioContain")}
+            >
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a
+              href="#ServiciosContain"
+              className={activeLink === "#ServiciosContain" ? "button" : ""}
+              onClick={() => handleLinkClick("#ServiciosContain")}
+            >
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a
+              href="#NosotrosContain"
+              className={activeLink === "#NosotrosContain" ? "button" : ""}
+              onClick={() => handleLinkClick("#NosotrosContain")}
+            >
+              Nosotros
+            </a>
+          </li>
+          <li>
+            <a
+              href="#ContactoContain"
+              className={activeLink === "#ContactoContain" ? "button" : ""}
+              onClick={() => handleLinkClick("#ContactoContain")}
+            >
+              Contacto
+            </a>
+          </li>
+        </ul>
+      )}
     </nav>
-  )
+  );
 }
